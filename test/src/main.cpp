9c92eb6d724774f8410671bce4787bf2122ead7d
jahnhtl <150966336+jahnhtl@users.noreply.github.com>
2025-11-17 14:10:58 +0100

    Change myFunction to accept a float parameter

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 // put function declarations here:
-int myFunction(int, int);
+int myFunction(int, float);
 
 void setup() {
   // put your setup code here, to run once:
@@ -13,6 +13,6 @@ void loop() {
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
+int myFunction(int x, float y) {
   return x + y;
 }
